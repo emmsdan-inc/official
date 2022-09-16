dd08bf775a42a63ebea27c806d21eb794b784c5b
Emmanuel Daniel <emmsdan.inc@gmail.com>
2022-09-16 11:35:36 +0100

    add github actions

---
+++ logo.sh
@@ -0,0 +1,19 @@
+
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+BLACKBACKGROUND='\033[40m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+STRING="WOW SO HURRY WOW SO HURRY WOW SO HURRY WOW SO HURRY WOW SO HURRY"
+NC='\033[0m'
+# No Color
+printf "${NC}\n\n"
+echo " __  __ ${GREEN} __        ${BLUE}Powered by ${GREEN}EmmsDan Industries"
+echo "${NC}|  |/  |${GREEN}|''|${ORANGE}.----.${RED}.-----.${CYAN}.---.-.${BLUE}.--.--.--.${MAGENTA}.---.-."
+echo "${NC}|░░░░ < ${GREEN}|░░|${ORANGE}|░░ _|${RED}|░░_░░|${CYAN}|░░_░░|${BLUE}|░░|░░|░░|${MAGENTA}|░░_░░|"
+printf "${NC}"
+echo "|__|\__|${GREEN}|__|${ORANGE}|__|  ${RED}|___  |${CYAN}|___:_|${BLUE}|________|${MAGENTA}|___:_|"
+echo "${RED}                  |_____|                       \n\n"
+
